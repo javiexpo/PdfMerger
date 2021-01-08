8225a18974cd67e2fe8aa1a37539970f428126d3
Javier Exposito <javiexpo2003@gmail.com>
2021-01-08 22:03:25 +0100

    Initial version 0.0.1

---
+++ main.py
@@ -0,0 +1,13 @@
+from PyPDF2 import PdfFileMerger, PdfFileReader
+ 
+# Call the PdfFileMerger
+mergedObject = PdfFileMerger()
+ 
+# I had 116 files in the folder that had to be merged into a single document
+# Loop through all of them and append their pages
+for fileNumber in range(1, 117):
+    mergedObject.append(PdfFileReader(str(fileNumber)+ '.pdf', 'rb'))
+    #mergedObject.append(PdfFileReader('6_yuddhakanda_' + str(fileNumber)+ '.pdf', 'rb'))
+ 
+# Write all the files into a file which is named as shown below
+mergedObject.write("mergedfilesoutput.pdf")
