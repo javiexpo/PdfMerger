aa93e18418cce1b9cc145b03b7f2c9ab10ab0e8c
Javier Exposito <javiexpo2003@gmail.com>
2021-01-09 23:37:40 +0100

    Adding raeding PDFs files from directory and merge

--- main.py
+++ main.py
@@ -1,13 +1,29 @@
 from PyPDF2 import PdfFileMerger, PdfFileReader
- 
+from os import listdir
+from os.path import isfile, join
+
+pathToPdfs = 'pdfs'
+onlyfiles = [f for f in listdir(pathToPdfs) if isfile(join(pathToPdfs, f))]
+
+fileNumbers = []
+for filePath in onlyfiles:
+    filePath = filePath.split('.', 1)
+    #print(filePath[0])
+    fileNumbers.append(int(filePath[0]));
+
+fileNumbers.sort()
+
+""" for n in fileNumbers:
+    print(n) """
+
 # Call the PdfFileMerger
 mergedObject = PdfFileMerger()
  
 # I had 116 files in the folder that had to be merged into a single document
 # Loop through all of them and append their pages
-for fileNumber in range(1, 117):
-    mergedObject.append(PdfFileReader(str(fileNumber)+ '.pdf', 'rb'))
+for fileNumber in fileNumbers:
+    mergedObject.append(PdfFileReader('pdfs/'+ str(fileNumber)+ '.pdf', 'rb'))
     #mergedObject.append(PdfFileReader('6_yuddhakanda_' + str(fileNumber)+ '.pdf', 'rb'))
  
 # Write all the files into a file which is named as shown below
-mergedObject.write("mergedfilesoutput.pdf")
+mergedObject.write("output.pdf")
